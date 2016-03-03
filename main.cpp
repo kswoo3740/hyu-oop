9c36f6d0b655bed1fd54203dee13dea2912e7ddc
Kim Sangwoo <kswoo3740@naver.com>
2016-03-04 00:07:13 +0900

    add model skeleton

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "models/person.h"
+
+using namespace std;
+
+int main(void) {
+    cout << "hello world" << endl;
+}
---
+++ models/person.h
@@ -0,0 +1,10 @@
+#include <string>
+
+using namespace std;
+
+class Person {
+    private:
+        int id;
+        string name;
+        string phone_number;
+};
